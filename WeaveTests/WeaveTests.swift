7cc716592aff23e57cb80d29305d8576b4f33a47
5310S <n07th3rn@proton.me>
2025-08-11 23:42:35 -0400

    Add files via upload

---
+++ WeaveTests/WeaveTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeaveTests.swift
+//  WeaveTests
+//
+//  Created by Keios on 8/10/25.
+//
+
+import Testing
+
+struct WeaveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
