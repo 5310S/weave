b803d727008588b216353920354e15c511b9f899
5310S <n07th3rn@proton.me>
2025-08-12 10:21:05 -0400

    Add files via upload

---
+++ weave/weaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  weaveApp.swift
+//  weave
+//
+//  Created by Keios on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct weaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
